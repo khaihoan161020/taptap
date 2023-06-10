75ed26d6bd316583d075452c50fea909fbbd9f2d
Hoan Truong <hoantruong@192.168.100.3>
2023-06-10 17:04:36 +0700

    test: save contact 7

--- src/components/Page/Home/SaveContact/index.tsx
+++ src/components/Page/Home/SaveContact/index.tsx
@@ -4,51 +4,40 @@ import Button from '@/components/Button'
 export default function SaveContact({ phone_number }: { phone_number: string }) {
     const handleSaveContact = () => {
         const contact = {
-            displayName: 'John Doe',
-            phoneNumbers: [
-                {
-                    value: '+1234567890',
-                    type: 'mobile'
-                }
-            ],
-            emailAddresses: [
-                {
-                    value: 'johndoe@example.com',
-                    type: 'work'
-                }
-            ]
-        }
-
-        let navigator: any = window.navigator
-        const supported = 'contacts' in navigator && 'ContactsManager' in window
-        console.log(supported, navigator)
-
-        alert(`supported ${supported}`)
-        if (supported) {
-            try {
-                navigator.contacts.save(
-                    contact,
-                    () => {
-                        console.log('Contact saved successfully!')
-                        alert('ok')
-                    },
-                    (error: any) => {
-                        console.error('Failed to save contact:', error)
-                        alert(`error: ${JSON.stringify(error)}`)
-                    }
-                )
-            } catch (error) {
-                alert(`error 2: ${error}`)
-            }
-        }
-        //   if (navigator && navigator?.contacts && navigator.contacts.create) {
-        //       navigator.contacts.save(contact, function () {
-        //         console.log("Contact saved successfully");
-        //       }, function (error: any) {
-        //         console.error("Error saving contact: " + error);
-        //       });
-        //   }
+            firstName: "John",
+            lastName: "Doe",
+            email: "johndoe@example.com",
+            phone: "+1234567890",
+          };
+        
+          const vCardData = generateVCard(contact);
+          downloadVCard(vCardData);
     }
+    function generateVCard(contact: any) {
+        const { firstName, lastName, email, phone } = contact;
+      
+        const vCard = `BEGIN:VCARD
+      VERSION:3.0
+      N:${lastName};${firstName};;;
+      FN:${firstName} ${lastName}
+      EMAIL:${email}
+      TEL:${phone}
+      END:VCARD`;
+      
+        return vCard;
+      }
+      
+      function downloadVCard(vCardData: any) {
+        const vCardBlob = new Blob([vCardData], { type: "text/vcard" });
+        const vCardURL = URL.createObjectURL(vCardBlob);
+      
+        const link = document.createElement("a");
+        link.href = vCardURL;
+        link.download = "contact.vcf";
+        link.click();
+      
+        URL.revokeObjectURL(vCardURL);
+      }
     return (
         <div className='sticky bottom-0 left-0 w-full py-1 flex justify-center'>
             {/* <a href={`tel:${phone_number}`} className="w-full inline-block" > */}
